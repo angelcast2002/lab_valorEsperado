4be37c8ecd9c5f624377f0ab8e4efee87ccc3536
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-03-15 23:20:03 -0600

    Update labValorEsperado.py

--- labValorEsperado.py
+++ labValorEsperado.py
@@ -1,12 +1,22 @@
-from sympy import symbols
-from sympy.stats import Poisson, density
-from sympy.stats import E, variance
+from sympy import symbols, exp, diff, EulerGamma
+from sympy.stats import Poisson, E, variance
 
 lam = symbols('lambda')
 lam = 0.5
+euler = EulerGamma.evalf()
+s = symbols('s')
 
 X = Poisson('X', lam)
 
 print(E(X))
 print(variance(X))
 
+
+funGeneradoraMomentos = E(exp(s*X))
+
+valorEsperado = funGeneradoraMomentos.diff(s).subs(s, 0)
+print(valorEsperado)
+
+#Teorema 9
+varianza = funGeneradoraMomentos.diff(s, 2).subs(s, 0) - valorEsperado**2
+print(varianza)
